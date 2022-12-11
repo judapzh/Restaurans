2483c5e48c8df5d3ebbe8fd9f69fa99815daf9a2
judapzh <89231720+judapzh@users.noreply.github.com>
2022-12-11 18:00:54 -0500

    acrual

--- my-app/src/navbar.js
+++ my-app/src/navbar.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-const navbar = () => {
+  
+const Navbar = () => {
 return (
     <div>
         <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
     <Link className="navbar-brand" href="#">Navbar</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
     <div cclassName="collapse navbar-collapse" id="navbarNavAltMarkup">
       <ul className="navbar-nav">
         <li className= "nav-item">
-        <Link className="nav-link" to ='/listar'>LISTADO</Link>
+        <Link className="nav-link" to ='/Listar'>LISTADO</Link>
         </li>
+     
         <li className= "nav-item">
         <Link className="nav-link" to ='/Search'>buscar</Link>
         </li>
@@ -30,4 +29,4 @@ return (
 </div>
 )
 }
-export default navbar;
+export default Navbar;
